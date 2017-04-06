dbee348cf053ba728d4e066a84ac5a8fe2e44418
guelzow <tobias.guelzow@brainworxx.de>
2017-04-06 11:34:13 +0200

    Release v2.2.0

--- Resources/Private/krexx/src/service/config/Fallback.php
+++ Resources/Private/krexx/src/service/config/Fallback.php
@@ -263,5 +263,5 @@ class Fallback
      *
      * @var string
      */
-    public $version = '2.2.0 dev';
+    public $version = '2.2.0';
 }
--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
   'title' => 'kreXX Debugger',
   'description' => 'kreXX is a feature rich php debugger with a neatly arranged debug window, featuring backend access to logfiles, code generation to reach the displayed values and much more.',
   'category' => 'misc',
-  'version' => '2.2.0 dev',
+  'version' => '2.2.0',
   'state' => 'stable',
   'uploadfolder' => 0,
   'clearCacheOnLoad' => 1,
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
   'author_company' => 'BRAINWORXX GmbH',
   'constraints' => array(
     'depends' => array(
-      'typo3' => '4.5.0-8.6.99',
+      'typo3' => '4.5.0-8.7.99',
       'php' => '5.3.0-7.1.99'
     ),
     'conflicts' => array(
