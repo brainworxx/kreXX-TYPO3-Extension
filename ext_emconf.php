425b8fffd753e695fcdd6c00d074fe7283e43987
guelzow <tobias.guelzow@brainworxx.de>
2017-09-02 11:42:35 +0200

    Updated ext_emconf to comply with the newly introduced mandatory fields of the TER.

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,6 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
   'author' => 'BRAINWORXX GmbH',
   'author_email' => 'tobias.guelzow@brainworxx.de',
   'author_company' => 'BRAINWORXX GmbH',
+  'createDirs' => '',
   'constraints' => array(
     'depends' => array(
       'typo3' => '4.5.0-8.7.99',
