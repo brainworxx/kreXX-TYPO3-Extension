ca13616929d2537b07a0f77b8faafba3f31a8cd8
guelzow <tobias.guelzow@brainworxx.de>
2020-02-24 15:48:05 +0100

    Updated the copyright year.

--- Classes/Controller/ConfigController.php
+++ Classes/Controller/ConfigController.php
@@ -18,7 +18,7 @@
  *
  *   GNU Lesser General Public License Version 2.1
  *
- *   kreXX Copyright (C) 2014-2019 Brainworxx GmbH
+ *   kreXX Copyright (C) 2014-2020 Brainworxx GmbH
  *
  *   This library is free software; you can redistribute it and/or modify it
  *   under the terms of the GNU Lesser General Public License as published by
