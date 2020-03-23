2bf5199f02c40e471412cc939b73d95954fd7793
guelzow <tobias.guelzow@brainworxx.de>
2020-03-23 15:17:36 +0100

    - Added backend display of the new log levels.

--- Resources/Public/JavaScript/Index.js
+++ Resources/Public/JavaScript/Index.js
@@ -262,6 +262,7 @@
      */
     ajaxRefresh.generateHtml = function (result) {
         var html = '';
+        var i;
 
         for (var key in result) {
             if (!result.hasOwnProperty(key)) {
@@ -269,12 +270,14 @@
             }
 
             var file = result[key];
-            html += '<tr ' + ajaxRefresh.generateBackgroundStyle(file.name) + '>';
-            html += '<td><a target="_blank" href="' + file.dispatcher + '"><div class="krexx-icon"></div></a></td>';
-            html += '<td><a target="_blank" href="' + file.dispatcher + '">  ' + file.name + '</a></td>';
+            html += '<tr ' + ajaxRefresh.generateBackgroundStyle(file.name) + '><td>';
+            for (i = 0; i < file.meta.length; i++) {
+                html += '<div class="krexx-icon ' + file.meta[i].level + '" title="' + file.meta[i].level + '"></div>';
+            }
+            html += '</td><td><a target="_blank" href="' + file.dispatcher + '">  ' + file.name + '</a></td>';
 
             html += '<td class="meta">';
-            for (var i = 0; i < file.meta.length; i++) {
+            for (i = 0; i < file.meta.length; i++) {
                 html += '<b>' + file.meta[i].type + '</b><br />';
                 html += 'in ' + file.meta[i].filename + ', line ' + file.meta[i].line;
                 if (i < file.meta.length - 1) {
