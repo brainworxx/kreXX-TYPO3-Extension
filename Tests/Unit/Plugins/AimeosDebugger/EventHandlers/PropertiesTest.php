75dbe13f543592898889942d1adedd59f6dc7ff6
guelzow <tobias.guelzow@brainworxx.de>
2021-12-11 11:01:25 +0100

    Additional unit tests for the aimeos property event handler.

--- Tests/Unit/Plugins/AimeosDebugger/EventHandlers/PropertiesTest.php
+++ Tests/Unit/Plugins/AimeosDebugger/EventHandlers/PropertiesTest.php
@@ -46,6 +46,7 @@ use Brainworxx\Krexx\Service\Plugin\Registration;
 use Brainworxx\Krexx\Service\Reflection\ReflectionClass;
 use Brainworxx\Krexx\Tests\Helpers\AbstractTest;
 use Brainworxx\Krexx\Tests\Helpers\RoutingNothing;
+use Aimeos\MW\View\Standard as StandardView;
 
 class PropertiesTest extends AbstractTest
 {
@@ -72,7 +73,7 @@ class PropertiesTest extends AbstractTest
      * @covers \Brainworxx\Includekrexx\Plugins\AimeosDebugger\EventHandlers\AbstractEventHandler::retrieveProperty
      * @covers \Brainworxx\Includekrexx\Plugins\AimeosDebugger\EventHandlers\Properties::dumpTheMagic
      */
-    public function testHandle()
+    public function testHandleItem()
     {
         $this->skipIfAimeosIsNotInstalled();
 
@@ -90,6 +91,8 @@ class PropertiesTest extends AbstractTest
 
         // Create a simple log item.
         $item = new \Aimeos\MAdmin\Log\Item\Standard($values);
+        $dynamicValue = 'dynamicValue';
+        $item->$dynamicValue = $dynamicValue;
         $fixture = [
             CallbackConstInterface::PARAM_DATA => $item,
             CallbackConstInterface::PARAM_REF => new ReflectionClass($item)
@@ -122,5 +125,58 @@ class PropertiesTest extends AbstractTest
             $this->assertEquals(CodegenConstInterface::CODEGEN_TYPE_PUBLIC, $models[$count]->getCodeGenType());
             ++$count;
         }
+
+        // Now for the dynamic value.
+        $this->assertEquals($dynamicValue, $models[$count]->getName());
+        $this->assertEquals($dynamicValue, $models[$count]->getData());
+        $this->assertEquals('->', $models[$count]->getConnectorLeft());
+        $this->assertEquals('', $models[$count]->getConnectorRight());
+        $this->assertEquals(CodegenConstInterface::CODEGEN_TYPE_PUBLIC, $models[$count]->getCodeGenType());
+    }
+
+    /**
+     * Pretty much the same as the item handler, but with a view object.
+     * And with a much lesser complexity, because the really deep stuff already
+     * got themselves tested.
+     *
+     * @covers \Brainworxx\Includekrexx\Plugins\AimeosDebugger\EventHandlers\Properties::handle
+     * @covers \Brainworxx\Includekrexx\Plugins\AimeosDebugger\EventHandlers\Properties::extractValues
+     * @covers \Brainworxx\Includekrexx\Plugins\AimeosDebugger\EventHandlers\AbstractEventHandler::retrieveProperty
+     * @covers \Brainworxx\Includekrexx\Plugins\AimeosDebugger\EventHandlers\Properties::dumpTheMagic
+     */
+    public function testHandleView()
+    {
+        $view = new StandardView();
+        $dynamicValue = 'dynamicValue';
+        $view->$dynamicValue = $dynamicValue;
+        $fixture = [
+            CallbackConstInterface::PARAM_DATA => $view,
+            CallbackConstInterface::PARAM_REF => new ReflectionClass($view)
+        ];
+
+        // Prevent the further routing.
+        Krexx::$pool->routing = new RoutingNothing(Krexx::$pool);
+
+        // Subscribing.
+        Registration::registerEvent(
+            PublicProperties::class . PluginConfigInterface::START_EVENT,
+            Properties::class
+        );
+        Krexx::$pool->eventService = new Event(Krexx::$pool);
+
+        // Run the test.
+        $publicProperties = new PublicProperties(Krexx::$pool);
+        $publicProperties->setParameters($fixture);
+        $this->triggerStartEvent($publicProperties);
+
+        // An asserting the results.
+        /** @var \Brainworxx\Krexx\Analyse\Model[] $models */
+        $model = Krexx::$pool->routing->model[0];
+
+        $this->assertEquals($dynamicValue, $model->getName());
+        $this->assertEquals($dynamicValue, $model->getData());
+        $this->assertEquals('->', $model->getConnectorLeft());
+        $this->assertEquals('', $model->getConnectorRight());
+        $this->assertEquals(CodegenConstInterface::CODEGEN_TYPE_PUBLIC, $model->getCodeGenType());
     }
 }
