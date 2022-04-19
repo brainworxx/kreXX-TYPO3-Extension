bf4b97f5e41210eb587b1be95b301510d488fd0c
guelzow <tobias.guelzow@brainworxx.de>
2022-04-19 12:55:19 +0200

    Bump copyright year.

--- Resources/Private/krexx/src/Analyse/Callback/Analyse/Objects/PublicProperties.php
+++ Resources/Private/krexx/src/Analyse/Callback/Analyse/Objects/PublicProperties.php
@@ -18,7 +18,7 @@
  *
  *   GNU Lesser General Public License Version 2.1
  *
- *   kreXX Copyright (C) 2014-2021 Brainworxx GmbH
+ *   kreXX Copyright (C) 2014-2022 Brainworxx GmbH
  *
  *   This library is free software; you can redistribute it and/or modify it
  *   under the terms of the GNU Lesser General Public License as published by
@@ -53,7 +53,6 @@ use ReflectionProperty;
  */
 class PublicProperties extends AbstractObjectAnalysis implements CallbackConstInterface
 {
-
     /**
      * Dump all public properties.
      *
