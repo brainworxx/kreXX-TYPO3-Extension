1bf56cff8411b5765fa0d5314cee980d5326e017
guelzowbap\guelzow <tobias.guelzow@brainworxx.de>
2022-12-03 10:19:28 +0100

    Simplified the extFilePath scalar analysis.

--- Classes/Plugins/Typo3/Scalar/ExtFilePath.php
+++ Classes/Plugins/Typo3/Scalar/ExtFilePath.php
@@ -63,9 +63,7 @@ class ExtFilePath extends FilePath
         }
 
         // Retrieve the EXT path from the framework.
-        set_error_handler(function (): void {
-            // Do nothing.
-        });
+        set_error_handler($this->pool->retrieveErrorCallback());
         try {
             $string = GeneralUtility::getFileAbsFileName($string);
         } catch (Throwable $e) {
@@ -81,16 +79,11 @@ class ExtFilePath extends FilePath
         }
 
         // Preserve the result from the getFileAbsFileName.
-        $model->addToJson(
-            $this->pool->messages->getHelp('TYPO3ResPath'),
-            $this->pool->fileService->filterFilePath($string)
-        );
+        $messages = $this->pool->messages;
+        $model->addToJson($messages->getHelp('TYPO3ResPath'), $this->pool->fileService->filterFilePath($string));
 
         if (!file_exists($string)) {
-            $model->addToJson(
-                $this->pool->messages->getHelp('TYPO3ResPathError'),
-                $this->pool->messages->getHelp('TYPO3ResPathDoesNotExist')
-            );
+            $model->addToJson($messages->getHelp('TYPO3ResPathError'), $messages->getHelp('TYPO3ResPathDoesNotExist'));
         }
 
         return parent::canHandle($string, $model);
