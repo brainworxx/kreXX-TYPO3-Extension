d9398cfc314a8e3c776c38d158e2a3cf4139eb81
guelzowbap\guelzow <tobias.guelzow@brainworxx.de>
2022-12-09 16:41:04 +0100

    Updated to TYPO3 12.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -45,7 +45,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'BRAINWORXX GmbH',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-12.0.99',
+            'typo3' => '7.6.0-12.1.99',
             'php' => '7.0.0-8.1.99',
         ],
         'conflicts' => [],
