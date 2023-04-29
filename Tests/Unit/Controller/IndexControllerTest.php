a0589a16d1492d5f03bfe75186ee3d65a085eed3
guelzowbap\guelzow <tobias.guelzow@brainworxx.de>
2023-04-29 11:18:25 +0200

    Added missing method mock.

--- Tests/Unit/Controller/IndexControllerTest.php
+++ Tests/Unit/Controller/IndexControllerTest.php
@@ -181,6 +181,9 @@ class IndexControllerTest extends AbstractTest
         $moduleTemplateMock->expects($this->once())
             ->method('setContent')
             ->with($templateContent);
+        $moduleTemplateMock->expects($this->once())
+            ->method('renderContent')
+            ->will($this->returnValue($templateContent));
 
         // Inject it, like there is no tomorrow.
         $this->indexController = new IndexController($configurationMock, $configFeMock, $settingsModel, $pageRenderer);
