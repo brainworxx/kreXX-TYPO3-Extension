3b707af5d92f55391e81e573a524ea81c82082f4
guelzowbap\guelzow <tobias.guelzow@brainworxx.de>
2025-05-01 09:09:28 +0200

    Results from unit test.

--- Classes/Plugins/AimeosDebugger/EventHandlers/Getter.php
+++ Classes/Plugins/AimeosDebugger/EventHandlers/Getter.php
@@ -234,19 +234,21 @@ class Getter extends AbstractEventHandler implements CallbackConstInterface
             }
         }
 
-        // Parse the code via regex, and retrieve default values.
         // Read the sourcecode into a string.
-        $sourcecode = $this->pool->fileService->readFile(
-            $reflectionMethod->getFileName(),
-            $reflectionMethod->getStartLine(),
-            $reflectionMethod->getEndLine()
-        );
-
         // Clean it up.
-        $sourcecode = str_replace(['(int)', '(string)', '(float)', '(bool)'], '', $sourcecode);
-        preg_match("/this->get\(\s*'([^']*)',\s*'([^']*)'\s*\);/", $sourcecode, $matches);
+        $sourcecode = str_replace(
+            ['(int)', '(string)', '(float)', '(bool)'],
+            '',
+            $this->pool->fileService->readFile(
+                $reflectionMethod->getFileName(),
+                $reflectionMethod->getStartLine(),
+                $reflectionMethod->getEndLine()
+            )
+        );
+        // Parse the code via regex, and retrieve default values.
+        preg_match("/this->get\(\s*'([^']*)',\s*([0-9]+)\s*\);/", $sourcecode, $matches);
         if (count($matches) === 3 && empty($result[$matches[1]])) {
-            $result[$matches[1]] = $matches[2];
+            $result[$matches[1]] = trim($matches[2], '"\'');
         }
 
         return $result;
